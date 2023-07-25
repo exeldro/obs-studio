7e05da517767e443ed6ea1f0516e983e6a0dbdb1
Ruwen Hahn <haruwenz@twitch.tv>
2023-07-25 12:22:58 +0200

    simulcast: Extract encoder/output creation to helper functions

--- plugins/simulcast/src/simulcast-output.cpp
+++ plugins/simulcast/src/simulcast-output.cpp
@@ -5,50 +5,63 @@
 
 #include "plugin-macros.generated.h"
 
+static OBSOutputAutoRelease create_output()
+{
+	OBSOutputAutoRelease output = obs_output_create(
+		"rtmp_output_simulcast", "rtmp simulcast", nullptr, nullptr);
+	if (!output) {
+		blog(LOG_ERROR, "failed to create simulcast rtmp output");
+		return nullptr;
+	}
+
+	obs_output_set_service(output, obs_frontend_get_streaming_service());
+	return output;
+}
+
+static OBSEncoderAutoRelease create_video_encoder(DStr &name_buffer,
+						  size_t encoder_index)
+{
+	dstr_printf(name_buffer, "simulcast video encoder %z", encoder_index);
+	OBSEncoderAutoRelease video_encoder = obs_video_encoder_create(
+		"jim_nvenc", name_buffer, nullptr, nullptr);
+	if (!video_encoder) {
+		blog(LOG_ERROR, "failed to create video encoder '%s'",
+		     name_buffer->array);
+		return nullptr;
+	}
+	obs_encoder_set_video(video_encoder, obs_get_video());
+	return video_encoder;
+}
+
+static OBSEncoderAutoRelease create_audio_encoder()
+{
+	OBSEncoderAutoRelease audio_encoder = obs_audio_encoder_create(
+		"ffmpeg_aac", "simulcast aac", nullptr, 0, nullptr);
+	if (!audio_encoder) {
+		blog(LOG_ERROR, "failed to create audio encoder");
+		return nullptr;
+	}
+	obs_encoder_set_audio(audio_encoder, obs_get_audio());
+	return audio_encoder;
+}
+
 void SimulcastOutput::StartStreaming()
 {
-	{
-		output_ = obs_output_create("rtmp_output_simulcast",
-					    "rtmp simulcast", nullptr, nullptr);
-		obs_output_set_service(output_,
-				       obs_frontend_get_streaming_service());
-		if (!output_) {
-			blog(LOG_ERROR,
-			     "failed to create simulcast rtmp output");
-			return;
-		}
-	}
+	output_ = create_output();
 
 	{
 		DStr name_buffer;
 		for (size_t i = 0; i < 1; i++) {
-			dstr_printf(name_buffer, "simulcast video encoder %z",
-				    i);
-			auto video_encoder = obs_video_encoder_create(
-				"jim_nvenc", name_buffer, nullptr, nullptr);
-			if (!video_encoder) {
-				blog(LOG_ERROR,
-				     "failed to create video encoder '%s'",
-				     name_buffer->array);
-				return;
-			}
-			obs_encoder_set_video(video_encoder, obs_get_video());
-			video_encoders_.push_back(video_encoder);
+			auto video_encoder =
+				create_video_encoder(name_buffer, i);
 			obs_output_set_video_encoder2(output_, video_encoder,
 						      i);
+			video_encoders_.push_back(std::move(video_encoder));
 		}
 	}
 
-	{
-		audio_encoder_ = obs_audio_encoder_create(
-			"ffmpeg_aac", "simulcast aac", nullptr, 0, nullptr);
-		if (!audio_encoder_) {
-			blog(LOG_ERROR, "failed to create audio encoder");
-			return;
-		}
-		obs_encoder_set_audio(audio_encoder_, obs_get_audio());
-		obs_output_set_audio_encoder(output_, audio_encoder_, 0);
-	}
+	audio_encoder_ = create_audio_encoder();
+	obs_output_set_audio_encoder(output_, audio_encoder_, 0);
 
 	streaming_ = true;
 }
