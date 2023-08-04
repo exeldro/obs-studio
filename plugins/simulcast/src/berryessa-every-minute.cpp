47aa16499d2c267b6262b4a15d2819093a33e470
Ruwen Hahn <haruwenz@twitch.tv>
2023-08-04 16:58:38 +0200

    simulcast: Add various OBS stats to every minute event

--- plugins/simulcast/src/berryessa-every-minute.cpp
+++ plugins/simulcast/src/berryessa-every-minute.cpp
@@ -7,14 +7,18 @@
 #include <QRandomGenerator>
 #include <QDateTime>
 
+static OBSFrameCounters InitFrameCounters();
+
 BerryessaEveryMinute::BerryessaEveryMinute(QObject *parent,
 					   BerryessaSubmitter *berryessa)
 	: QObject(parent),
 	  berryessa_(berryessa),
 	  presentmon_(this),
 	  timer_(this),
-	  startTime_(QDateTime::currentDateTimeUtc())
+	  startTime_(QDateTime::currentDateTimeUtc()),
+	  frame_counters_(InitFrameCounters())
 {
+	obs_cpu_usage_info_.reset(os_cpu_usage_info_start());
 
 	connect(&timer_, &QTimer::timeout, this, &BerryessaEveryMinute::fire);
 
@@ -27,6 +31,60 @@ BerryessaEveryMinute::BerryessaEveryMinute(QObject *parent,
 
 BerryessaEveryMinute::~BerryessaEveryMinute() {}
 
+static OBSFrameCounters InitFrameCounters()
+{
+	OBSFrameCounters frame_counters{};
+
+	auto video = obs_get_video();
+	frame_counters.skipped = video_output_get_skipped_frames(video);
+	frame_counters.output = video_output_get_total_frames(video);
+
+	frame_counters.rendered = obs_get_total_frames();
+	frame_counters.lagged = obs_get_lagged_frames();
+
+	return frame_counters;
+}
+
+static void AddOBSStats(os_cpu_usage_info *info,
+			OBSFrameCounters &frame_counters, obs_data_t *event)
+{
+	obs_data_set_double(event, "obs_cpu_usage",
+			    os_cpu_usage_info_query(info));
+	obs_data_set_double(event, "current_obs_fps", obs_get_active_fps());
+	obs_data_set_int(event, "obs_memory_usage",
+			 os_get_proc_resident_size());
+
+	obs_data_set_int(event, "total_memory", os_get_sys_total_size());
+	obs_data_set_int(event, "free_memory", os_get_sys_free_size());
+
+	// FIXME: these return 0 currently for plugin streams
+	auto video = obs_get_video();
+	auto skipped_frames = video_output_get_skipped_frames(video);
+	auto output_frames = video_output_get_total_frames(video);
+
+	auto rendered_frames = obs_get_total_frames();
+	auto lagged_frames = obs_get_lagged_frames();
+
+	auto maybe_update = [](uint32_t &holder, uint32_t val) {
+		if (holder > val)
+			holder = val;
+	};
+	maybe_update(frame_counters.output, output_frames);
+	maybe_update(frame_counters.skipped, skipped_frames);
+	maybe_update(frame_counters.rendered, rendered_frames);
+	maybe_update(frame_counters.lagged, lagged_frames);
+
+	obs_data_set_int(event, "skipped_frames",
+			 skipped_frames - frame_counters.skipped);
+	obs_data_set_int(event, "output_frames",
+			 output_frames - frame_counters.output);
+
+	obs_data_set_int(event, "rendered_frames",
+			 rendered_frames - frame_counters.rendered);
+	obs_data_set_int(event, "lagged_frames",
+			 lagged_frames - frame_counters.lagged);
+}
+
 void BerryessaEveryMinute::fire()
 {
 	blog(LOG_INFO, "BerryessaEveryMinute::fire called");
@@ -34,6 +92,8 @@ void BerryessaEveryMinute::fire()
 	OBSDataAutoRelease event = obs_data_create();
 
 	presentmon_.summarizeAndReset(event);
+
+	AddOBSStats(obs_cpu_usage_info_.get(), frame_counters_, event);
 
 	berryessa_->submit("ivs_obs_stream_minute", event);
 
--- plugins/simulcast/src/berryessa-every-minute.hpp
+++ plugins/simulcast/src/berryessa-every-minute.hpp
@@ -7,7 +7,23 @@
 
 #include "presentmon-csv-capture.hpp"
 
+#include <util/platform.h>
+
+#include <memory>
+
 class BerryessaSubmitter;
+
+struct OSCPUUsageInfoDeleter {
+	void operator()(os_cpu_usage_info *info)
+	{
+		os_cpu_usage_info_destroy(info);
+	}
+};
+
+struct OBSFrameCounters {
+	uint32_t output, skipped;
+	uint32_t rendered, lagged;
+};
 
 class BerryessaEveryMinute : public QObject {
 	Q_OBJECT
@@ -23,4 +39,9 @@ private:
 	PresentMonCapture presentmon_;
 	QTimer timer_;
 	QDateTime startTime_;
+
+	std::unique_ptr<os_cpu_usage_info, OSCPUUsageInfoDeleter>
+		obs_cpu_usage_info_;
+
+	OBSFrameCounters frame_counters_;
 };
