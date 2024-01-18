5b60006198cdc856c6ab96d5f7b6d8d9b8bd7f79
Ruwen Hahn <haruwenz@twitch.tv>
2024-01-18 18:43:27 +0100

    FIXME rtmp-services: Always copy server info to settings

--- plugins/rtmp-services/rtmp-common.c
+++ plugins/rtmp-services/rtmp-common.c
@@ -134,6 +134,8 @@ static const char *get_protocol(json_t *service, obs_data_t *settings)
 	return "RTMP";
 }
 
+static void copy_info_to_settings(json_t *service, obs_data_t *settings);
+
 static void rtmp_common_update(void *data, obs_data_t *settings)
 {
 	struct rtmp_common *service = data;
@@ -184,6 +186,8 @@ static void rtmp_common_update(void *data, obs_data_t *settings)
 				service->ertmp_config_url =
 					bstrdup(ertmp_config_url);
 			}
+
+			copy_info_to_settings(serv, settings);
 
 			json_t *rec = json_object_get(serv, "recommended");
 			if (json_is_object(rec)) {
@@ -553,6 +557,17 @@ static void update_protocol(json_t *service, obs_data_t *settings)
 	obs_data_set_string(settings, "protocol", "RTMP");
 }
 
+static void copy_info_to_settings(json_t *service, obs_data_t *settings)
+{
+	fill_more_info_link(service, settings);
+	fill_stream_key_link(service, settings);
+	copy_string_from_json_if_available(service, settings,
+					   "ertmp_configuration_info_link");
+	copy_string_from_json_if_available(service, settings,
+					   "ertmp_multitrack_video_name");
+	update_protocol(service, settings);
+}
+
 static inline json_t *find_service(json_t *root, const char *name,
 				   const char **p_new_name)
 {
@@ -619,13 +634,8 @@ static bool service_selected(obs_properties_t *props, obs_property_t *p,
 	}
 
 	fill_servers(obs_properties_get(props, "server"), service, name);
-	fill_more_info_link(service, settings);
-	fill_stream_key_link(service, settings);
-	copy_string_from_json_if_available(service, settings,
-					   "ertmp_configuration_info_link");
-	copy_string_from_json_if_available(service, settings,
-					   "ertmp_multitrack_video_name");
-	update_protocol(service, settings);
+	copy_info_to_settings(service, settings);
+
 	return true;
 }
 
